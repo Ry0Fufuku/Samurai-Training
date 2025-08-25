fb65a9940323352e34332d4ae8c5b393c3c710fd
oyama <oyama@DEML>
2025-08-25 16:13:09 +0900

    API記入前

--- samuraitravel/src/main/java/com/example/samuraitravel/form/ReservationRegisterForm.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/form/ReservationRegisterForm.java
@@ -1,5 +1,15 @@
 package com.example.samuraitravel.form;
 
-public class ReservationRegisterForm {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class ReservationRegisterForm {
+	private Integer houseId;
+	private Integer userId;
+	private String checkinDate;
+	private String checkoutDate;
+	private Integer numberOfPeople;
+	private Integer amount;
 }
